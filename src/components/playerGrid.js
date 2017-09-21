ac293e7b6dad7d6e7e61a6b04a6026c6d7ebad21
msolorio <msolorio789@gmail.com>
2017-09-20 22:56:35 -0700

    create players board from opponents board

--- src/components/playerGrid.js
+++ src/components/playerGrid.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {clickBox, randomlyPlaceShips} from '../actions';
+import './playerGrid.css';
 
-export default function PlayerGrid(props) {
-  return (
-    <div className="PlayerGrid">
-      from PlayerGrid component
-    </div>
-  );
+export class PlayerGrid extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.props.dispatch(randomlyPlaceShips());
+  }
+
+  onBoxClick(e, rowIndex, colIndex) {
+    this.props.dispatch(clickBox(rowIndex, colIndex));
+  }
+
+  getBoxClassName(value) {
+    switch(value) {
+      case 0:
+        return 'untouched-empty';
+      case 1:
+        return 'untouched-occupied-player';
+      case 2:
+        return 'miss';
+      case 3:
+        return 'hit';
+      default:
+        console.error('class name unknown');
+    }
+  }
+
+  getColumns(row, rowIndex) {
+    return row.map((column, colIndex) => {
+
+      return (
+        <span className={`column ${this.getBoxClassName(column)}`}
+          key={colIndex}
+          onClick={(e) => this.onBoxClick(e, rowIndex, colIndex)}>
+        </span>
+      );
+    });
+  }
+
+  getRows() {
+    return this.props.opponentGrid.map((row, rowIndex) => {
+      return (
+        <div className="row"
+          key={rowIndex}>
+          {this.getColumns(row, rowIndex)}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="PlayerGrid">
+        {this.getRows()}
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({...state});
+
+export default connect(mapStateToProps)(PlayerGrid);
